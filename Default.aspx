7176a102ddf7f4ab475b48640914de19ecb61e4f
unknown <Poveda@Poveda-PC.(none)>
2011-10-17 20:46:34 -0200

    Send for github

---
+++ Default.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true"
+    CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<%@ Register Assembly="Microsoft.ReportViewer.WebForms, Version=10.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a"
+    Namespace="Microsoft.Reporting.WebForms" TagPrefix="rsweb" %>
+
+<asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
+</asp:Content>
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+    <h2>
+        Welcome to ASP.NET!
+    </h2>
+    <p>
+        To learn more about ASP.NET visit <a href="http://www.asp.net" title="ASP.NET Website">www.asp.net</a>.
+    </p>
+    <p>
+        You can also find <a href="http://go.microsoft.com/fwlink/?LinkID=152368&amp;clcid=0x409"
+            title="MSDN ASP.NET Docs">documentation on ASP.NET at MSDN</a>.
+    </p>
+
+    <rsweb:ReportViewer ID="rv" runat="server">
+</rsweb:ReportViewer>
+    <br />
+    <br />
+    <br />
+    <asp:GridView ID="GridView1" runat="server">
+    </asp:GridView>
+    <br />
+
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        dsViewVendasTableAdapters.VendasTableAdapter tableAdapter = new dsViewVendasTableAdapters.VendasTableAdapter();
+        dsViewVendas.VendasDataTable dataTable = new dsViewVendas.VendasDataTable();
+        dataTable.Merge(tableAdapter.MeuSelect());
+
+        DataSet ds = new DataSet();
+        DataTable t = dataTable;
+        
+
+        Microsoft.Reporting.WebForms.ReportDataSource item = new Microsoft.Reporting.WebForms.ReportDataSource();
+        item.Name = "dsViewVendas_VendasTableAdapter";
+        item.Value = dataTable;
+
+        rv.ProcessingMode = Microsoft.Reporting.WebForms.ProcessingMode.Local;
+        rv.LocalReport.ReportPath = Server.MapPath("~/Report2.rdlc");
+        //rv.LocalReport.DataSources.Add(item);
+        rv.LocalReport.DataSources.Add(new Microsoft.Reporting.WebForms.ReportDataSource("dsViewVendas_VendasDataTable", (DataTable)dataTable));
+        rv.DataBind();
+
+        GridView1.DataSource = dataTable;
+        GridView1.DataBind();
+    }
+
+    private DataSet GetVendas()
+    {
+        DataSet ds = new DataSet();
+        
+        SqlConnection conn = new SqlConnection("Data Source=POVEDA-PC\\SQLEXPRESS;Initial Catalog=TestingReport;Integrated Security=True");
+        SqlCommand command = new SqlCommand("Select * From Vendas", conn);
+        SqlDataAdapter daVendas = new SqlDataAdapter(command);
+        daVendas.Fill(ds, "Vendas");
+        return ds;
+    }
+}
